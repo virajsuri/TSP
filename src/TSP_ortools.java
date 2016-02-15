873d43cc9aab348fe387e70ab2b22e6285e796aa
Viraj Suri <vpsuri@irusmail.com>
2016-02-15 11:15:36 -0500

    test message again

--- src/TSP_ortools.java
+++ src/TSP_ortools.java
@@ -16,7 +16,7 @@ import com.google.ortools.constraintsolver.NodeEvaluator2;
 import com.google.ortools.constraintsolver.RoutingModel;
 
 class TSP_ortools {
-	//test
+	//testTEST
 	static String fileName = "eil51.tsp", line = "START",tspName = fileName.substring(0, fileName.length() - 4);
 	static int size = 0;
 	static boolean openLink=true;
